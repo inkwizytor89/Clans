e05ef0ed210e2efb5c3126dd7ada86e2159f1d73
Kamil Kostyszyn <inkwizytor89@gmail.com>
2016-12-23 16:24:54 +0100

    Add hash password in database

--- db/reinstall_db.sh
+++ db/reinstall_db.sh
@@ -3,13 +3,13 @@
 . ../project.properties
 
 for FILE in `ls --ignore-backups unpatch | sort -r`; do 
-    sudo -u postgres psql -t -d $db_name -f "unpatch/$FILE"; 
+    sudo -u postgres psql --quiet -t -d $db_name -f "unpatch/$FILE"; 
 done
 
 for FILE in patch/*.sql; do 
-	sudo -u postgres psql -t -d $db_name -f "$FILE";
+	sudo -u postgres psql --quiet -t -d $db_name -f "$FILE";
 done
 
 for FILE in init_data/*.sql; do 
-	sudo -u postgres psql -t -d $db_name -f "$FILE";
+	sudo -u postgres psql --quiet -t -d $db_name -f "$FILE";
 done
